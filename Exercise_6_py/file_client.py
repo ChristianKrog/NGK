56fa2f8b96f98c0a73f1499c4bc882104251cb9e
root <christiankrog@hotmail.dk>
2019-09-23 14:22:34 +0200

    start på exe6 - uden send/recieve

--- Exercise_6_py/file_client.py
+++ Exercise_6_py/file_client.py
@@ -4,9 +4,18 @@ from lib import Lib
 
 PORT = 9000
 BUFSIZE = 1000
+serverName = 'clientTCP'
 
 def main(argv):
 	# TO DO Your Code
+	clientSock = socket(AF_INET, SOCK_STREAM)
+	clientSock.connect((serverName,PORT))
+
+	userInput = raw_input("Please enter a valid filename: ")
+	Lib.writeTextTCP(userInput,clientSock)
+	Lib.getFileSizeTCP(clientSock)
+
+
     
 def receiveFile(fileName,  conn):
 	# TO DO Your Code
--- Exercise_6_py/file_server.py
+++ Exercise_6_py/file_server.py
@@ -2,15 +2,27 @@ import sys
 import socket
 from lib import Lib
 
-HOST = ''
+HOST = '10.0.0.1'
 PORT = 9000
 BUFSIZE = 1000
 
 def main(argv):
 	# TO DO Your Code
+	serverSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	serverSock.bind("",PORT)
+	serverSock.listen(1)
+	print 'The server is ready to receive'
+	conn, addr = serverSock.accept()
+
+	txtClient = Lib.readTextTCP(serverSock)
+	
+	size = Lib.check_File_Exist(txtClient)
+
+	Lib.writeTextTCP(size, serverSock)
 
 def sendFile(fileName,  fileSize,  conn):
 	# TO DO Your Code
+
     
 if __name__ == "__main__":
    main(sys.argv[1:])
