7cfd24d82259d6ed5111a861d744f75f35ce0c51
root <christiankrog@hotmail.dk>
2019-09-23 15:39:56 +0200

    client virker ikke socket.error [errno 111]

--- Exercise_6_py/file_client.py
+++ Exercise_6_py/file_client.py
@@ -1,25 +1,22 @@
 import sys
-from socket import *
+import socket 
 from lib import Lib
 
-PORT = 9000
+HOST = '10.0.0.2'
+PORT = 80
 BUFSIZE = 1000
-serverName = 'clientTCP'
 
 def main(argv):
 	# TO DO Your Code
-	clientSock = socket(AF_INET, SOCK_STREAM)
-	clientSock.connect((serverName,PORT))
+	clientSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	clientSock.connect((HOST,PORT))
 
 	userInput = raw_input("Please enter a valid filename: ")
 	Lib.writeTextTCP(userInput,clientSock)
 	Lib.getFileSizeTCP(clientSock)
 
-
-    
 #def receiveFile(fileName,  conn):
 	# TO DO Your Code
-
 
 if __name__ == "__main__":
    main(sys.argv[1:])
