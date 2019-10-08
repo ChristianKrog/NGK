d03afe741f573d492fff7abe8848a035b4d7e5cc
root <christiankrog@hotmail.dk>
2019-10-08 09:29:03 +0200

    server up and runnning!

--- exercise_7/server/UDP_server.py
+++ exercise_7/server/UDP_server.py
@@ -1,32 +1,38 @@
 import sys
 from socket import * 
-from lib import Lib
 
 HOST = '10.0.0.1'
 PORT = 9000
-addr= (HOST, PORT)
+serverAddr= (HOST, PORT)
 BUF = 1024
 
-uptime = '/proc/uptime'
-loadavg = '/proc/loadavg'
+uptime = "/proc/uptime"
+loadavg = "/proc/loadavg"
 
 
 def main(argv):
 	# TO DO Your Code
 	serverSock = socket(AF_INET, SOCK_DGRAM)
-	serverSock.bind(addr)
+	serverSock.bind(serverAddr)
 
 	while True:
+		print 'The server is ready to receive'
 		data = serverSock.recvfrom(BUF)
+		letter = data[0]
+		clientAddr = data[1]
+		print 'Letter: "' + letter +'" received from client with address: ' + format(clientAddr)
 
-		if data == ('U' || 'u')
-			serverSock.sendto(uptime, addr)
+		if letter  == 'U' or letter  == 'u':
+			file1 = open(uptime,"r+")
+			fileData1 = file1.read()
+			serverSock.sendto(fileData1, clientAddr)
+			print 'Uptime sent'
 
-		else if data == ('L' || 'l')
-			serverSock.sendto(loadavg, addr)
-
-		else 
-			print 'error'
+		elif letter  == 'L' or letter == 'l':
+			file2 = open(loadavg,"r+")
+			fileData2 = file2.read()
+			serverSock.sendto(fileData2, clientAddr)
+			print 'Loadavg sent'
 
     
 if __name__ == "__main__":
