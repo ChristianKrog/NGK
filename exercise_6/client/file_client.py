6b05aeb2f5760a13064d820ced2a93c3a17185b9
root <christiankrog@hotmail.dk>
2019-10-08 13:52:33 +0200

    fix af repo

--- exercise_6/client/file_client.py
+++ exercise_6/client/file_client.py
@@ -1,22 +1,43 @@
 import sys
-import socket 
+from socket import *
 from lib import Lib
 
-HOST = '10.0.0.1'
+HOST = '10.0.0.1' #server IP
 PORT = 9000
 BUFSIZE = 1000
 
 def main(argv):
-	# TO DO Your Code
-	clientSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	clientSock.connect((HOST,PORT))
+	fileName = sys.argv[1] #input from terminal when compiling
+	clientSock = socket(AF_INET, SOCK_STREAM) #create tcp socket
+	clientSock.connect((HOST,PORT)) #connect socket to the server address 
 
-	userInput = raw_input("Please enter a valid filename: ")
-	Lib.writeTextTCP(userInput,clientSock)
-	Lib.getFileSizeTCP(clientSock)
+	print("Connected to server")
 
-#def receiveFile(fileName,  conn):
-	# TO DO Your Code
+	Lib.writeTextTCP(fileName,clientSock) #writes filename to server
+	
+	receiveFile(fileName, clientSock) #calls recieveFile()
 
+def receiveFile(fileName,  conn):
+	fileSize = Lib.getFileSizeTCP(conn) 
+	if fileSize == 0: #checks if file exist
+		print("Filesize is 0. File doesn't exist")
+		conn.close() #close connection to the server
+	else:
+		print 'Filesize is: ', fileSize
+		print("Starting download")
+
+	f = open("fileName", "wb") #open file or create. write binary. 
+	receivedData = conn.recv(BUFSIZE) #Reads the first 1000 bytes sent from server
+	f.write(receivedData) #write the first chunk of the file
+	receivedTotal = len(receivedData) #sets the total number of bytes recieved 
+		
+	while receivedTotal < fileSize: #runs until total number of recieved bytes is larger or equal to filesize
+		receivedData = conn.recv(BUFSIZE) #reads chunks of 1000 bytes sent from server
+		f.write(receivedData) #writes the received data to file 
+		receivedTotal += len(receivedData) #keeps count on the total number og bytes received. 
+	
+	print("Dowload completed. Client will close")
+	conn.close() #close connection to server. 
+		
 if __name__ == "__main__":
    main(sys.argv[1:])
--- exercise_6/server/file_server.py
+++ exercise_6/server/file_server.py
@@ -2,39 +2,40 @@ import sys
 from socket import * 
 from lib import Lib
 
-HOST = '10.0.0.1'
+HOST = '10.0.0.1' #server IP
 PORT = 9000
 BUFSIZE = 1000
 
 def main(argv):
-	# TO DO Your Code
-	serverSock = socket(AF_INET, SOCK_STREAM)
-	serverSock.bind((HOST,PORT))
-	serverSock.listen(1)
+	serverSock = socket(AF_INET, SOCK_STREAM) #create TCP socket
+	serverSock.bind((HOST,PORT)) #Bind socket to IP og port(address)
+	serverSock.listen(1) #waiting for client to connect. 
 	print 'The server is ready to receive'
 	
 	while True: 
-		conn, addr = serverSock.accept()
+		conn, addr = serverSock.accept() #completes connection with client
 		print 'Connected to client - Port - ', PORT
-		txtClient = Lib.readTextTCP(conn)
-		fileName = Lib.extractFilename(txtClient)
-		fileSize = str(Lib.check_File_Exists(fileName))
-		Lib.writeTextTCP(fileSize, conn)
-		sendFile(fileName,fileSize,conn)
+		txtClient = Lib.readTextTCP(conn) #Reads file and directory from client
+		fileName = Lib.extractFilename(txtClient) 
+		fileSize = str(Lib.check_File_Exists(fileName)) 
+		Lib.writeTextTCP(fileSize, conn) #send filesize to client. 
+		sendFile(fileName,fileSize,conn) #calls sendfile()
 
 
 def sendFile(fileName,  fileSize,  conn):
-	# TO DO Your Code
-	f = open(fileName, 'rb')
-	dataTosend = f.read(BUFSIZE)
+	f = open(fileName, 'rb') #opens the file for binary read. 
+	dataTosend = f.read(BUFSIZE) #reads the first 1000 bytes
 	
 	while dataTosend: 
-		conn.send(dataTosend)
+		conn.send(dataTosend) #sends file in chunks of 1000 bytes
 		
-		dataTosend = f.read(BUFSIZE)
+		dataTosend = f.read(BUFSIZE) #reads chunks of 1000 bytes until finished
 	print('Done sending')
-	f.close()
+	f.close() #close file
 
     
 if __name__ == "__main__":
    main(sys.argv[1:])
+
+
+
